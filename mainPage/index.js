e1575ad2407010b3ce997f57a8d28d80ec231da7
ShahlaAliyeva <raufa5740@gmail.com>
2022-02-14 13:37:22 +0400

    bugs was fixed

--- mainPage/index.js
+++ mainPage/index.js
@@ -8,11 +8,28 @@
 // }, 3000);
 
 //Responsive Navbar
-
-$('.menu').click(function () {
-    $('.links').toggleClass("responsive");
-    return false;
-});
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      navbar.classList.add("show");
+      menuBtn.classList.add("hide");
+      body.classList.add("disabled");
+    }
+    cancelBtn.onclick = ()=>{
+      body.classList.remove("disabled");
+      navbar.classList.remove("show");
+      menuBtn.classList.remove("hide");
+    }
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+  
+// $('.menu').click(function () {
+//     $('.links').toggleClass("responsive");
+//     return false;
+// });
 
 
 //Login form
