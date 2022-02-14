344a3cd44af9a554482dcd229635751bbe771f1b
ShahlaAliyeva <raufa5740@gmail.com>
2022-02-14 14:31:19 +0400

    bugs fixed

--- mainPage/index.js
+++ mainPage/index.js
@@ -1,35 +1,18 @@
-// var counter = 1;
-// setInterval(function() {
-//     document.getElementById('radio' + counter).checked = true;
-//     counter++;
-//     if(counter > 4) {
-//         counter = 1;
-//     }
-// }, 3000);
+var counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4) {
+        counter = 1;
+    }
+}, 3000);
 
 //Responsive Navbar
-const body = document.querySelector("body");
-    const navbar = document.querySelector(".navbar");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      body.classList.add("disabled");
-    }
-    cancelBtn.onclick = ()=>{
-      body.classList.remove("disabled");
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-    }
-    window.onscroll = ()=>{
-      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-    }
-  
-// $('.menu').click(function () {
-//     $('.links').toggleClass("responsive");
-//     return false;
-// });
+
+$('.menu').click(function () {
+    $('.links').toggleClass("responsive");
+    return false;
+});
 
 
 //Login form
